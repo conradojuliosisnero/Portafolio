e9593d84a2fb0360dcc7ed6c643af77a77f44e64
conrado julio <conradojuliosisinero@gmail.com>
2023-06-21 19:59:34 -0500

    añadiendo la libreria de scroll

--- main.js
+++ main.js
@@ -17,7 +17,6 @@ modoDark.addEventListener('click',()=>{
 });
 
 // comprovacion del localstore 
-
 if (localStorage.getItem('oscuro') === 'true') {
   document.body.classList.add('modo-oscuro');
   nav.classList.add('nav-dark');
@@ -31,4 +30,88 @@ if (localStorage.getItem('oscuro') === 'true') {
 
 }
 
+window.sr = ScrollReveal();
 
+// navegador 
+sr.reveal('.navegador',{
+  duration: 3000,
+  origin: 'bottom',
+  distance:'-100px' 
+});
+
+// titulo del navegador 
+
+sr.reveal('.navagador__titulo',{
+  duration: 4000,
+  origin: 'top',
+  distance:'100px' 
+});
+// titulo de bienvenida 
+sr.reveal('.bienvenida__titulo',{
+  duration: 2000,
+  origin: 'left',
+  distance:'100px' 
+});
+
+// boton de contacto bienvenida
+sr.reveal('.boton__contactar',{
+  duration: 2000,
+  origin: 'bottom',
+  distance:'50px' 
+});
+
+// imagen principal 
+sr.reveal('.contenedor__foto',{
+  duration: 2000,
+  origin: '',
+  distance:'250px' 
+});
+
+// redes sociales 
+sr.reveal('.redes__sociales',{
+  duration: 2000,
+  origin: 'bottom',
+  distance:'250px' 
+});
+
+// imagen about 
+sr.reveal('.scroll__img',{
+  duration: 3000,
+  origin: '',
+  distance:'100px' 
+});
+
+// titulo about 
+sr.reveal('.scroll__titulo',{
+  duration:3000,
+  origin:'top',
+  distance:'50px'
+})
+
+// estadisticas about 
+sr.reveal('.scroll__stast',{
+  duration:3000,
+  origin:'top',
+  distance:'50px'
+})
+
+// btn about 
+sr.reveal('.about__btn',{
+  duration:3000,
+  origin:'bottom',
+  distance:'50px'
+})
+
+// skill__card 
+
+sr.reveal('.skill__card',{
+  duration:2000,
+  origin:'bottom',
+  distance:'-300px'
+})
+// proyectos 
+sr.reveal('.card',{
+  duration:3000,
+  origin:'bottom',
+  distance:'-300px'
+})
